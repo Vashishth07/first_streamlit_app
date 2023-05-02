d18d0c936f6171308f0b014ee845b2eeeb1b5b9f
Vashishth07 <132057741+Vashishth07@users.noreply.github.com>
2023-05-02 06:50:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,9 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(fruits_to_show)
 #streamlit.header("Fruityvice Fruit Advice!")
 def get_fruityvice_data(this_fruit_choice):
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-return (fruityvice_normalized)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return (fruityvice_normalized)
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?',)
